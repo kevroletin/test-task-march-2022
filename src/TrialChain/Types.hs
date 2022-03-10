0a8a7e1e35de9d94dd2fcf1a7847cf037f1c24c7
Vasiliy Kevroletin <kevroletin@gmail.com>
2022-03-10 11:48:35 +1000

    Prettify json instances

--- src/TrialChain/Types.hs
+++ src/TrialChain/Types.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module TrialChain.Types
   ( Hash,
     mkHash,
@@ -12,11 +14,22 @@ module TrialChain.Types
   )
 where
 
+import Data.Aeson
+import Data.Aeson.TH (Options (fieldLabelModifier), defaultOptions, deriveJSON)
 import Data.Binary
 import qualified Data.Text.Encoding.Base16 as Base16
 import Protolude
 
-newtype Hash = Hash {unHash :: Text} deriving (Show, Eq, Ord)
+newtype Hash = Hash {unHash :: Text}
+  deriving stock (Generic)
+  deriving newtype (Show, Eq, Ord, ToJSON)
+
+instance FromJSON Hash where
+  parseJSON (String str) =
+    case mkHash str of
+      Left _ -> mzero
+      Right x -> pure x
+  parseJSON _ = mzero
 
 mkHash :: Text -> Either Text Hash
 mkHash str =
@@ -29,19 +42,19 @@ mkHashUnsafe = Hash
 
 newtype Signature = Signature {unSignature :: Text}
   deriving stock (Generic)
-  deriving newtype (Binary, Show, Eq, Ord)
+  deriving newtype (Binary, Show, Eq, Ord, FromJSON, ToJSON)
 
 newtype PublicKey = PublicKey {unPublicKey :: Text}
   deriving stock (Generic)
-  deriving newtype (Binary, Show, Eq, Ord)
+  deriving newtype (Binary, Show, Eq, Ord, FromJSON, ToJSON)
 
 newtype PrivateKey = PrivateKey {unPrivateKey :: Text}
   deriving stock (Generic)
-  deriving newtype (Binary, Show, Eq, Ord)
+  deriving newtype (Binary, Show, Eq, Ord, FromJSON, ToJSON)
 
 newtype Money = Money {unMoney :: Integer}
   deriving stock (Generic)
-  deriving newtype (Binary, Show, Eq, Ord)
+  deriving newtype (Binary, Show, Eq, Ord, FromJSON, ToJSON)
 
 data TxBody = TxBody
   { txb_from :: PublicKey,
@@ -57,3 +70,7 @@ data Tx = Tx
     tx_signature :: Signature
   }
   deriving (Generic, Show, Eq, Ord)
+
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 3} ''Tx)
+
+$(deriveJSON defaultOptions {fieldLabelModifier = drop 4} ''TxBody)
